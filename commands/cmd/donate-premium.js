5b1c49d607d41c92db5909803f62f778b60f8e36
Devil Dev <84636715+MAPXOR12@users.noreply.github.com>
2022-07-28 15:45:35 +0300

    Update donate-premium.js

--- commands/cmd/donate-premium.js
+++ commands/cmd/donate-premium.js
@@ -1,11 +1,11 @@
-const { ownerID } = ("../../config.js");
+const ownerid = "872563022344892426";
 
 module.exports = {
     name: 'turnoff',
     aliases: ['shutdown', 'close'],
 
     async execute(client, message) {
-        if (message.author.id !== ownerID) return;
+        if (message.author.id !== ownerid) return;
         message.channel.send(`Shutting down...`);
         return process.exit(1);
     }
