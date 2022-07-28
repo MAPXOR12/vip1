3774aaf9cafb05b8a6feaccecad1fc63eb315eaf
Devil Dev <84636715+MAPXOR12@users.noreply.github.com>
2022-07-28 15:42:22 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 exports.Prefix = `-`;//your prefix for bot
 exports.Token = `OTE0MDgyMzE3MzQ0MDU5NDAy.GXDvMT.Ssjt50AeG1eNZTEJ5iGb_lf2qHfZ1qK26u9Znk`;//your token 
 exports.Color = `#303136`;//color   of embed
+exports.ownerID = `743887896481628190`;
