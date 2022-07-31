1caf1dea97def1b088c7e071fdbafcb91fd75041
Devil Dev <84636715+MAPXOR12@users.noreply.github.com>
2022-07-31 12:54:00 +0300

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -21,7 +21,7 @@ Avatar, Coinflip, Howgay, Meme, Rate, 8ball, Dicksize, Ascii, Choose, Hack, Rand
 **Moderation**
 Clear, Mute, Unmute, Tempmute, Kick, Ban, Unban, Tempban, Warn, Warnings, ResetWarns
 **Gif-Anime**
-Boy, Anime
+Boy, feed 
 **Information**
 Help, Covid, Weather, Userinfo, Serverinfo, Ping`)
     .setFooter(`Requested By ${message.author.username}`)
