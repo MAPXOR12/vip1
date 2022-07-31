1f9e162e2fb82f65eb9e4435277ef8012d1ff1cf
Devil Dev <84636715+MAPXOR12@users.noreply.github.com>
2022-07-31 12:49:00 +0300

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -17,7 +17,7 @@ module.exports = {
     .setTitle(`${client.user.username} Commands!`)
     .setDescription(`Use ${Prefix}Help <Command Name> For More Command Information!` + 
     "\n\n**Fun**\n`Avatar, Coinflip, Howgay, Meme, Rate, 8ball, Dicksize, Ascii, Choose, Hack, Randomnumber`" + "\n\n" + "**Moderation**\n`Clear, Mute, Unmute, Tempmute, Kick, Ban, Unban, Tempban, Warn, Warnings, ResetWarns`" + 
-    "**Gif-Anime**\n`Boy, Anime`" "\n\n" +
+    "**Gif-Anime**\n`Boy, Anime`" +
     "**Information**\n`Help, Covid, Weather, Userinfo, Serverinfo, Ping`")
     .setFooter(`Requested By ${message.author.username}`)
     .setTimestamp();
